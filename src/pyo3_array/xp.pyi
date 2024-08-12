951ed823eba72d1a128e4ab48b43778f72a27123
Lucas Colley <lucas.colley8@gmail.com>
2024-08-12 17:25:35 +0100

    STY: appease linter

--- src/pyo3_array/xp.pyi
+++ src/pyo3_array/xp.pyi
@@ -8,6 +8,11 @@ if TYPE_CHECKING:
     device = Any
 
 def eye(
-    n_rows: int, n_cols: int | None, /, *,
-    k: int | None, dtype: dtype | None, device: device | None,
+    n_rows: int,
+    n_cols: int | None,
+    /,
+    *,
+    k: int | None,
+    dtype: dtype | None,
+    device: device | None,
 ) -> Array: ...
